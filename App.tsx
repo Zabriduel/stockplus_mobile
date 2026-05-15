639e9cb300da70fe01e5913bb2323334ebba2961
mariabigati <mariasozua345@gmail.com>
2026-05-15 09:12:46 -0300

    First commitLotes Vencidos

--- App.tsx
+++ App.tsx
@@ -1,12 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import { StyleSheet, Text, View } from 'react-native';
+import LotesVencimento from "./src/screens/LotesVencer/LotesVencer";
+
+export type RootStackParamList = {
+  LotesVencimento: undefined
+}
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='LotesVencimento' component={LotesVencimento}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+
+   
   );
 }
 
